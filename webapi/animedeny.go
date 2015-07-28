f0f092a8e8c7aef843665a19580c73b22fbd800d
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-07-28 10:14:36 +0900

    addDeny test

--- webapi/animedeny.go
+++ webapi/animedeny.go
@@ -17,6 +17,10 @@ type Conf struct {
 	UserName string
 }
 
+type DenyList struct {
+	List []string
+}
+
 func CreateConfExample() {
 	c := Conf{Token: "", UserName: "slackbot"}
 	js, err := json.Marshal(c)
@@ -54,6 +58,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
 }
 
+func addDenyDB(str []string) {
+	r, err := ioutil.ReadFile("../database/deny.db")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var readList DenyList
+	err = json.Unmarshal(r, &readList)
+	if err != nil {
+		log.Println("db is empty.")
+		log.Println(err)
+	}
+	readList.List = append(readList.List, str...)
+
+	w, err := json.Marshal(readList)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	ioutil.WriteFile("../database/deny.db", w, 0644)
+}
+
 func animedeny(w http.ResponseWriter, r *http.Request) {
 	text := r.PostFormValue("text")
 	token := r.PostFormValue("token")
@@ -72,6 +96,11 @@ func animedeny(w http.ResponseWriter, r *http.Request) {
 			attachments := slack.NewAttachments(1)
 			if strings.HasPrefix(text, "deny:") {
 				text = "true"
+				var str []string
+				str = append(str, "hoge")
+				str = append(str, "huga")
+				str = append(str, "hage")
+				addDenyDB(str)
 			} else {
 				text = "false"
 			}
@@ -89,7 +118,7 @@ func animedeny(w http.ResponseWriter, r *http.Request) {
 			ioutil.ReadAll(resp.Body)
 			defer resp.Body.Close()
 
-			fmt.Fprintf(w, "hoge")
+			fmt.Fprintf(w, "")
 
 			/*
 				fmt.Println("outgoing-webhook: " + configure.UserName)
