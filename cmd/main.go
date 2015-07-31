adc20ef22c2dde2f687a866bee1b4c36090eabd4
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-08-01 01:54:56 +0900

    deny Command 実装

--- cmd/main.go
+++ cmd/main.go
@@ -5,44 +5,24 @@ import (
 
 	"github.com/RobotClubKut/SlackBot/lib/mysql"
 	"github.com/RobotClubKut/SlackBot/lib/slack"
+	"github.com/RobotClubKut/SlackBot/lib/webapi"
 )
 
 func main() {
-	//_, err := ioutil.ReadFile("test.go")
-	//log.TerminateAndWriteMessage(err, "hoge")
-
-	//noSub := nosub.GetNosubUpdate()
-	//mysql.InsertNoSubData(noSub)
-	//js, _ := json.Marshal(n)
-	//fmt.Println(string(js))
-	//nosub.GetAnimeData("slack_bot")
-	//couchbase.InsertDataBase(noSub)
-
-	//var word []string
-	//word = append(word, "test")
-	//word = append(word, "にゃんぱす")
-	//mysql.InsertDenyWord(word)
-
-	//fmt.Println(mysql.GetAnimeMostNewAnime())
-	//a := slack.NewAttachments(1)
-	//a.Attachments[0].Text = "test"
-	//js, _ := json.Marshal(a)
-	//slack.Post(string(js))
-	//mysql.InsertNoSubBufData(noSub)
-	//fmt.Println(mysql.DiffNoSubData("[720p]だんちがい 第04話 「四号棟 カレーライス」"))
-	/*
-		for _, d := range mysql.CreatePostNoSubData() {
-			fmt.Println(d)
-		}
-	*/
-	ch := make(chan bool)
+	chSlack := make(chan bool)
+	chWebAPI := make(chan bool)
 	go func() {
 		for {
 			slack.PostAnimeInfomation(mysql.CreatePostNoSubData())
 			time.Sleep(10 * time.Minute)
 		}
 
-		ch <- true
+		chSlack <- true
 	}()
-	<-ch
+	go func() {
+		webapi.ViewWebPage()
+		chWebAPI <- true
+	}()
+	<-chSlack
+	<-chWebAPI
 }
---
+++ lib/webapi/webapi.go
@@ -0,0 +1,49 @@
+package webapi
+
+// webapiだお
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/RobotClubKut/SlackBot/lib/conf"
+	"github.com/RobotClubKut/SlackBot/lib/mysql"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
+}
+
+func deny(w http.ResponseWriter, r *http.Request) {
+	configure := conf.ReadConfigure()
+	text := r.PostFormValue("text")
+	token := r.PostFormValue("token")
+	userName := r.PostFormValue("user_name")
+	if strings.Contains(text, "deny:") && configure.OutgoingSlackConf.Token == token && configure.OutgoingSlackConf.UserName == userName {
+		text = strings.Replace(text, "deny:", "", 0)
+		text = strings.Replace(text, " ", "", 0)
+		words := strings.Split(text, ":")
+		mysql.InsertDenyWord(words)
+		//postText := "{\"text\":\"" +  + ""\"}"
+		postText := "{\"text\":\""
+		for _, w := range words {
+			if w != "" {
+				postText += w
+				postText += ","
+			}
+		}
+		postText += "\"}"
+		fmt.Fprintf(w, postText)
+	} else {
+		postText := "{\"text\":\"nilぱすー\"}"
+		fmt.Fprintf(w, postText)
+	}
+}
+
+//ViewWebPage is view web api
+func ViewWebPage() {
+	configure := conf.ReadConfigure()
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":"+configure.OutgoingSlackConf.Port, nil)
+}
